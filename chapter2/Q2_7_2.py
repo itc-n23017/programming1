62901bb7d47773cc560fd365ef380080a7aad0c4
seito sugama <n23017@std.it-college.ac.jp>
2023-07-06 07:50:34 +0000

    chapter2

---
+++ chapter2/Q2_7_2.py
@@ -0,0 +1,4 @@
+my_list = ["orange", "apple", "greap", "banana"]
+my_list.sort()
+print(my_list)#昇順
+
---
+++ chapter2/Q2_7_2_2.py
@@ -0,0 +1,3 @@
+my_list = ["orange", "apple", "greap", "banana"]
+my_list.sort(reverse=True)
+print(my_list)  # 降順
