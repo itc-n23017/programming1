0f36d94418cc4ad29954c4b383cd7b93ea79aa19
seito sugama <n23017@std.it-college.ac.jp>
2023-08-15 07:49:16 +0000

    chapter5

---
+++ chapter5/Q5_7_1.py
@@ -0,0 +1,2 @@
+yamanote_line = {0: "Tokyo", 1: "Yuurakuchou", 2: "Shinbashi"}
+print(*[v for v in yamanote_line.values()], sep="\n")
---
+++ chapter5/Q5_7_2.py
@@ -0,0 +1,15 @@
+data = [
+    ["0001", "Male", "Yamada", "Tarou", 25, "Tokyo"],
+    ["0002", "Male", "Satou", "Takeshi", 27, "Kanagawa"],
+    ["0003", "Female", "Tanaka", "Yuko", 25, "Saitama"],
+    ["0004", "Male", "Suzuki", "Ichiro", 35, "Hokkaido"],
+]
+member = {}
+for record in data:
+    key = record[0]
+    info = record[1:]
+    member[key] = info
+
+print("number", "information", sep="\t")
+for key, info in member.items():
+    print(key, info)
